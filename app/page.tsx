017c92e3efffd46547c8ff188bbaf3b00204ef04
Karen Valenzuela Landero <11952598+karenvalenzuelalandero@users.noreply.github.com>
2024-01-31 21:45:42 -0600

    Dashboard funcionando

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,6 @@
-
+import { Card, Text, Title } from "@tremor/react";
+import Search from "@/components/Search";
+import UsersTable from "@/components/UsersTable";
 
 type Props = {
   searchParams: {
@@ -20,5 +22,14 @@ export default async function Home({ searchParams }: Props) {
       },
     },
   });
-  return <main>home</main>;
+  return (
+    <main className="p-4 md:p-10 mx-auto max-w-7xl">
+      <Title>Users</Title>
+      <Text>A table of users retrieved from our Postgres database.</Text>
+      <Search />
+      <Card className="mt-6">
+        <UsersTable users={users} />
+      </Card>
+    </main>
+  );
 }
--- components/Search.tsx
+++ components/Search.tsx
@@ -5,18 +5,17 @@ import { useTransition } from "react";
 import { RotateCwIcon, SearchIcon } from "lucide-react";
 
 type Props = {
-  query?: string;
+  disabled?: boolean;
 };
 
-export default function Search({ query }: Props) {
+export default function Search({ disabled }: Props) {
 
-  const router = useRouter();
+  const { replace } = useRouter();
   const pathname = usePathname();
   const [isPending, startTransition] = useTransition();
+  const params = new URLSearchParams(window.location.search);
 
   function handleSearch(value: string) {
-    const params = new URLSearchParams(window.location.search);
-
     if (value) {
       params.set("q", value);
     } else {
@@ -24,7 +23,7 @@ export default function Search({ query }: Props) {
     }
 
     startTransition(() => {
-      router.replace(`${pathname}?${params.toString()}`);
+      replace(`${pathname}?${params.toString()}`);
     });
   }
 
@@ -34,8 +33,8 @@ export default function Search({ query }: Props) {
         Search
       </label>
       <div className="rounded-md shadow-sm">
-        <div className="pointer-events-none absolute inset-y-0 left-0 flex items-center pl-3">
-          <SearchIcon className="mr-3 h-4 w-4 text-gray-400" />
+        <div className="pointer-events-none absolute inset-y-0 left-0 flex items-center pl-3" aria-hidden="true">
+          <SearchIcon className="mr-3 h-4 w-4 text-gray-400" aria-hidden="true" />
         </div>
         <input
           type="text"
@@ -45,14 +44,14 @@ export default function Search({ query }: Props) {
           className="h-10 block w-full rounded-md border border-gray-200 pl-9 focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
           placeholder="Search by name..."
           onChange={(event) => handleSearch(event.target.value)}
-          defaultValue={query}
+          defaultValue={params.get("q") ?? ""}
         />
       </div>
 
       {isPending && (
-    <div className="absolute right-0 top-0 bottom-0 flex items-center justify-center">
-      <RotateCwIcon className="animate-spin -ml-1 mr-3 h-5 w-5 text-gray-700" />
-    </div>
+        <div className="absolute right-0 top-0 bottom-0 flex items-center justify-center">
+          <RotateCwIcon className="animate-spin -ml-1 mr-3 h-5 w-5 text-gray-700" />
+        </div>
   )}
 
     </div>
